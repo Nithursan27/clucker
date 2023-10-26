8a1bc7a87795bf1c2ad12ca1d09a6e943a7be43a
Nithursan <nithursan.muraleetharan@kcl.ac.uk>
2023-10-26 15:23:46 +0100

    Add feed and user sign in functionality

--- microblogs/tests/test_user_model.py
+++ microblogs/tests/test_user_model.py
@@ -1,8 +1,10 @@
+"""Unit tests for the User model"""
 from django.test import TestCase
-from .models import User
+from microblogs.models import User
 from django.core.exceptions import ValidationError
 
 class UserModelTestCase(TestCase):
+    """Unit tests for the User model"""
     def setUp(self):
         self.user = User.objects.create_user(
             "@johndoe",
