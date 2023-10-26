773fb6bbde4b67f0f51cfe15fe5c98b7773d1fa9
Nithursan <nithursan.muraleetharan@kcl.ac.uk>
2023-10-26 17:48:10 +0100

    Process successful and unsuccessful logins

--- microblogs/views.py
+++ microblogs/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect, render
 from .forms import SignUpForm, LogInForm
+from django.contrib.auth import authenticate, login
 
 def home(request):
     return render(request, "home.html")
@@ -18,5 +19,14 @@ def feed(request):
     return render(request,"feed.html")
 
 def log_in(request):
+    if request.method == "POST":
+        form = LogInForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(username=username,password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("feed")
     form = LogInForm()
     return render(request,"log_in.html", {"form": form})
