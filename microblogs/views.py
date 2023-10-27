08bde263e6f283190be55ea73c7b451114919937
Nithursan <nithursan.muraleetharan@kcl.ac.uk>
2023-10-27 11:28:15 +0100

    Add log out functionality

--- microblogs/views.py
+++ microblogs/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import redirect, render
 from .forms import SignUpForm, LogInForm
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 def home(request):
@@ -33,3 +33,7 @@ def log_in(request):
         messages.add_message(request, messages.ERROR,"Incorrect login")
     form = LogInForm()
     return render(request,"log_in.html", {"form": form})
+
+def log_out(request):
+    logout(request)
+    return redirect("home")
